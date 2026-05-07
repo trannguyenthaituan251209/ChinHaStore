af73f95471a4577061b8a3dc47fdfebac40af9cc
ThaiTuan <thaituantrannguyen@gmail.com>
2026-05-07 14:45:01 +0700

    feat: add HeroSection component and update site SEO metadata tags

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,12 +6,11 @@ const HeroSection = () => {
     <section className="hero">
       <div className="container hero-container">
         <h1 className="hero-title">
-          Dịch vụ cho thuê máy <br /> ảnh chất lượng cao
+          Thuê Máy Ảnh Buôn Ma Thuột <br /> Dịch Vụ Chuyên Nghiệp
         </h1>
         <p className="hero-subtitle">
-          Dịch vụ cho thuê máy ảnh - máy quay chuyên nghiệp, chất lượng cao và giá cả tốt nhất<br />
-          thị trường ChinHaStore. Hỗ trợ học sinh - sinh viên thuê không cần cọc thế chấp. Giá<br />
-          thuê chỉ từ 180.000VND/Ngày
+          Dịch vụ cho thuê máy ảnh, máy quay chuyên nghiệp tại Buôn Ma Thuột (BMT) với giá tốt nhất<br />
+          thị trường. Hỗ trợ HSSV thuê không cần cọc thế chấp. Giá thuê chỉ từ 180.000VND/Ngày.
         </p>
       </div>
       <div className="hero-image-wrapper">
